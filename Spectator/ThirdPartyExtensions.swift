d5dd4a5a69019949cd7675e5b5569cf8909a21d6
Michael Zeller <mike@mikezeller.net>
2016-11-29 14:13:50 -0500

    remove dead code

--- Spectator/ThirdPartyExtensions.swift
+++ Spectator/ThirdPartyExtensions.swift
@@ -5,31 +5,3 @@
 //  Created by Michael Zeller on 7/5/16.
 //  Copyright © 2016 Lights and Shapes. All rights reserved.
 //
-
-import SwiftyJSON
-// MARK: Extensions
-
-extension JSON {
-    /**
-     Dictonary mapping of String:String specific to Twitches API data
-     */
-    var dictOfString: [String:String]? {
-        get {
-            switch self.type {
-            case .dictionary:
-                var mapped: [String:String] = [:]
-                var gen = self.makeIterator()
-                while let x = gen.next() {
-                    if let value = x.1.string {
-                        mapped[x.0] = value
-                    } else {
-                        return nil
-                    }
-                }
-                return mapped
-            default:
-                return nil
-            }
-        }
-    }
-}
