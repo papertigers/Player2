75b8ffb76dbe10e5fa4685d67a7ad5f75b502332
Mike Zeller <mike@mikezeller.net>
2017-04-01 01:47:29 -0400

    Add some settings tab tableview styling

--- Spectator/Settings/SettingsViewController.swift
+++ Spectator/Settings/SettingsViewController.swift
@@ -11,10 +11,15 @@ import UIKit
 class SettingsViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     @IBOutlet weak var tableView: UITableView!
     
-    let general =  [Settings.login]
-    let experience = [Settings.theme]
-    let about = [Settings.acknowledgements]
+    enum Section: Int {
+        case general = 0, experience, about
+    }
     
+    let settings: [Section: [Settings]] = [
+        .general: [Settings.login],
+        .experience: [Settings.theme],
+        .about: [Settings.acknowledgements]
+    ]
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -28,14 +33,54 @@ class SettingsViewController: UIViewController, UITableViewDelegate, UITableView
     }
     
     // MARK: - Settings TableView
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return settings.count
+    }
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 20
+        return self.settings[Section(rawValue: section)!]!.count
+    }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        var title: String = ""
+        switch Section(rawValue: section)! {
+        case .general:
+            title = "General"
+        case .experience:
+            title = "Experience"
+        case .about:
+            title = "About"
+        }
+        
+        return title
+    }
+    
+    func tableView(_ tableView: UITableView, willDisplayHeaderView view: UIView, forSection section: Int) {
+        let header = view as? UITableViewHeaderFooterView
+        header?.textLabel?.textColor = .lightGray
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = UITableViewCell()
-        cell.textLabel?.text = "Testing"
+        let settingsArray = self.settings[Section(rawValue: indexPath.section)!]!
+        cell.textLabel?.text = settingsArray[indexPath.row].text
+        cell.textLabel?.textColor = .white
+        
         return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didUpdateFocusIn context: UITableViewFocusUpdateContext, with coordinator: UIFocusAnimationCoordinator) {
+        if let prevIndexPath = context.previouslyFocusedIndexPath {
+            let prevCell = tableView.cellForRow(at: prevIndexPath)
+            prevCell?.contentView.backgroundColor = .clear
+            prevCell?.textLabel?.textColor = .white
+        }
+        
+        if let nextIndexPath = context.nextFocusedIndexPath {
+            let nextCell = tableView.cellForRow(at: nextIndexPath)
+            nextCell?.contentView.backgroundColor = .white
+            nextCell?.textLabel?.textColor = .black
+        }
     }
 
     /*
